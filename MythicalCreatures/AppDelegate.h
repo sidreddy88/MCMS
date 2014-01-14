a8d58ca433d6a83d95670fdc0166e82d182df90e
Tony Severino <tony@professional-painters.com>
2014-01-14 14:27:42 -0600

    Initial Commit

---
+++ MythicalCreatures/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MythicalCreatures
+//
+//  Created by Anthony  Severino on 1/14/14.
+//  Copyright (c) 2014 Simple Management Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MythicalCreatures/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MythicalCreatures
+//
+//  Created by Anthony  Severino on 1/14/14.
+//  Copyright (c) 2014 Simple Management Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
